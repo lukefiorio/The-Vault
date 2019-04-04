e5f36c258a638ef144c271647c7fd210821d3388
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-03 18:26:58 -1000

    cleaned formatting

--- vault.js
+++ vault.js
@@ -2,6 +2,7 @@
 module.exports = function() {
   
   let vault = {};
+  
   const setValue = function(key,value) {
     if (!key || !value) {
       return null;
@@ -11,44 +12,20 @@ module.exports = function() {
   }
 
   const getValue = function(key) {
+    // // alternatively
+    // if (!vault.key) return null;
+
+    // return vault.key;
+
     if (vault.key) {
       return vault.key;
     } else {
       return null;
     }
   }
-
   return {
     setValue: setValue,
     getValue: getValue
   }
 
 };
-
-
-/*
-
-setValue( key, value )
-
-accepts two arguments `key` and `value`.  
-will set the `value` in the vault identified by it's `key`.  
-
-```
-getValue( key )
-
-accepts one argument `key`.  
-will return the `value` that was set if the `key` exists in the vault.  
-will return `null` if the `key` does not exist in the vault.
-
-*/
-
-/*
-var myVault = vault();
-console.log( myVault.getValue() ); // null
-console.log( myVault.setValue('secret password', 'hunter2') );
-console.log( myVault.getValue('secret password') ); // hunter2
-console.log( myVault.getValue('bank account') ); // null
-
-var yourVault = vault();
-console.log( myVault.getValue('secret password') ); // null
-*/
